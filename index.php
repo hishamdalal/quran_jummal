2077feb5c8af0513c693ef2c792ed6f84e419c83
Hisham Dalal <hishamdalal@hotmail.com>
2022-09-01 02:20:36 -0700

    format html quran website
    make quran website link next to convert button.

--- index.php
+++ index.php
@@ -31,9 +31,12 @@ include_once 'jummal.class.php';
 					placeholder="قُلْ أَعُوذُ بِرَبِّ النَّاسِ ﴿1﴾ مَلِكِ النَّاسِ ﴿2﴾ إِلَٰهِ النَّاسِ ﴿3﴾ مِنْ شَرِّ الْوَسْوَاسِ الْخَنَّاسِ ﴿4﴾ الَّذِي يُوَسْوِسُ فِي صُدُورِ النَّاسِ ﴿5﴾ مِنَ الْجِنَّةِ وَالنَّاسِ ﴿6﴾"
 				><?=$jummal->get_verses_str()?></textarea>
 			</div>
-			<div><input value="<?=__('Convert')?>" type="submit" /></div>
+			<div>
+				<input value="<?=__('Convert')?>" type="submit" />
+				<a target="_blank" href="https://equran.me">موقع القرآن</a>
+			</div>
 		</form>
-		<a href="https://equran.me">موقع القرآن</a>
+		
 		<br />
 
 		<?php $jummal->render(); ?>
